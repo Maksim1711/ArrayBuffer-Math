1a5c2fe3eae011cac778d0ca3309b4a0afb6dc23
Maksim Oloviannikov <olovjanqnet@gmail.com>
2022-10-12 14:33:12 +0300

    added arrayBuffer-Math

---
+++ src/js/__test__/Magician.test.js
@@ -0,0 +1,13 @@
+import Character from '../Magician';
+
+test('create Character ok', () => {
+  const character = new Character('Rick', 'Magician');
+  const result = {
+    name: 'Rick',
+    type: 'Magician',
+    health: 100,
+    level: 1,
+    defence: 40,
+  };
+  expect(character).toMatchObject(result);
+});
